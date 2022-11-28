5f84ed54702f31b14c0423fb761a41aecb5c3468
Aigerim <abdulakimovaaigerim205@gmail.com>
2022-11-28 16:58:53 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+
+
+            try {
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.println("Enter a number in the parallelepiped");
+                    float height = Float.parseFloat(scanner.nextLine());
+                    float length = Float.parseFloat(scanner.nextLine());
+                    float width = Float.parseFloat(scanner.nextLine());
+                    Parallelepiped p= new Parallelepiped(height, length, width);
+                    System.out.println(p);
+
+                    System.out.println("Area of parallelepiped: " + p.area());
+                    System.out.println("Volume of parallelepiped: " + p.volume());
+
+            }catch (ArithmeticException e){
+                    throw new NumberFormatException();
+            }
+
+
     }
 }
